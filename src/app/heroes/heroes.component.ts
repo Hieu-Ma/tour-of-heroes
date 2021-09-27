5b7712baf41f3fbd636b0c86cb44dbc857b9ad83
Hieu-Ma <simshieu1@gmail.com>
2021-09-27 12:47:50 -0700

    debugging DI

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -12,12 +12,16 @@ export class HeroesComponent implements OnInit {
   selectedHero?: Hero;
   heroes = Hero[] = [];
   
-  constructor() { }
+  constructor(private heroService: HeroService) { }
   
   ngOnInit() {
   }
   
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+  }
+
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
   }
 }
